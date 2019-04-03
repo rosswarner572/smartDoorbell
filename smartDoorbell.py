4b06fe33af119b60b7a40859eac6fa050ced9ef7
rosswarner572 <39916015+rosswarner572@users.noreply.github.com>
2019-04-03 15:09:40 -0400

    Create smartDoorbell.py

---
+++ smartDoorbell.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2.7
+import telegram
+import RPi.GPIO as GPIO
+from time import sleep
+from os import system
+from pygame import mixer
+import logging
+
+tokenID = '637044422:AAHwS6DYxFlTu6UMZO3BfY6K1nx9jEn5RZg'
+bot = telegram.Bot(token=tokenID)
+chatID = 541823565
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    while True:
+        input_state = GPIO.input(17)
+        if input_state == False:
+            bot.sendMessage(chat_id=chatID, text="Doorbell is ringing")
+            system("omxplayer -o alsa /home/pi/Music/doorbell-1.wav")
+            #sleep(1.0)
+            continue
+
+finally:
+    bot.sendMessage(chat_id=chatID, text="doorbell is offline")
+    GPIO.cleanup()
+
+
